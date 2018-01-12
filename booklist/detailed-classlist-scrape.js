95691e39951d6f173a3c6631cca5dd06b3ae6b7f
Chris Hallberg <crhallberg@gmail.com>
2018-01-12 14:21:06 -0500

    Scrape professor names and emails.

--- booklist/detailed-classlist-scrape.js
+++ booklist/detailed-classlist-scrape.js
@@ -1,5 +1,7 @@
+var emaillinks = document.querySelectorAll('a[href*="mailto"]');
 var rows = document.querySelectorAll(".datadisplaytable tr");
 var classes = [];
+ei = 0;
 for (var i = 0; i < rows.length; i++) {
 	var header = rows[i].querySelector(".ddlabel,.ddtitle");
 	if (!header) {
@@ -14,10 +16,13 @@ for (var i = 0; i < rows.length; i++) {
 	}
 	var booksLink = rows[i].querySelector("td a").href;
 	classes.push({
-		name: name,
+		name: name.trim(),
 		students: enrollment,
 		booklist: booksLink,
+		prof_name: emaillinks[ei].getAttribute("target"),
+		prof_email: emaillinks[ei].getAttribute("href").substr(7),
 	});
+	ei += 1;
 }
 document.querySelector(".banner_copyright").innerHTML = JSON.stringify(classes);
 "done"
