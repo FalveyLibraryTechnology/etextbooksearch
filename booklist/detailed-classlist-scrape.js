7c41d05b1b1361e4226137264d268a09972bd8f2
Chris Hallberg <crhallberg@gmail.com>
2018-01-16 15:50:59 -0500

    Escape urls.

--- booklist/detailed-classlist-scrape.js
+++ booklist/detailed-classlist-scrape.js
@@ -1,28 +1,37 @@
-var emaillinks = document.querySelectorAll('a[href*="mailto"]');
 var rows = document.querySelectorAll(".datadisplaytable tr");
 var classes = [];
-ei = 0;
+lastIndex = -1;
 for (var i = 0; i < rows.length; i++) {
 	var header = rows[i].querySelector(".ddlabel,.ddtitle");
 	if (!header) {
+		if (lastIndex > -1) {
+			var emaillink = rows[i].querySelector('a[href*="mailto"]');
+			if (emaillink) {
+				classes[lastIndex].prof_name = emaillink.getAttribute("target");
+				classes[lastIndex].prof_email = emaillink.getAttribute("href").substr(7);
+			} else {
+				classes[lastIndex].prof_name = "n/a";
+				classes[lastIndex].prof_email = "n/a";
+			}
+			lastIndex = -1;
+		}
 		continue;
 	}
-	var name = header.querySelector("a").innerText;
+	var code = header.querySelector("a").innerText;
 	var enrollment = rows[i].querySelector("th");
 	if (enrollment.querySelector("b")) {
 		enrollment = parseInt(enrollment.querySelector("b").innerText.match(/\d+/)[0]);
 	} else {
 		enrollment = parseInt(enrollment.innerText.split("Enrollment: ")[1].match(/^\d+/)[0]);
 	}
-	var booksLink = rows[i].querySelector("td a").href;
+	var booksLink = rows[i].querySelector("td a").href.replace(/&amp;/g, "&");
 	classes.push({
-		name: name.trim(),
+		code: code.trim(),
 		students: enrollment,
 		booklist: booksLink,
-		prof_name: emaillinks[ei].getAttribute("target"),
-		prof_email: emaillinks[ei].getAttribute("href").substr(7),
 	});
-	ei += 1;
+	lastIndex = classes.length - 1;
 }
 document.querySelector(".banner_copyright").innerHTML = JSON.stringify(classes);
+console.log(classes.length);
 "done"
